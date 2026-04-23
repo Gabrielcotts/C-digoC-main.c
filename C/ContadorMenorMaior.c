3bcf5ab43bf23b6e5af95972fbaf504cf380f87b
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-23 11:58:05 -0300

    Add files via upload

---
+++ C/ContadorMenorMaior.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main(){
+	
+Contador do menor para o maior :
+
+
+ int num = 0;
+
+printf("Digite um numero: ");
+scanf("%i", &num);
+
+for(int i = 1; i <= num; i++){
+	printf("%d \n", i);
+	}
+}
