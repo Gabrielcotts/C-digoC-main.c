224ed99ce8458a964da34bd156e7ce8cb3f29ddf
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-23 12:01:06 -0300

    Add files via upload

---
+++ C/NotasAlunos.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+	
+ float nota;
+	printf("Digite sua nota: ");
+	scanf("%f", &nota);
+	
+	
+	if(nota >= 9 && nota <= 10){
+		printf("ss \n");
+	} else if (nota <= 8.9 && nota >= 7){
+		printf("ms\n");	
+	} else if (nota <= 6.9 && nota >= 5) {
+		printf("mm\n");
+	} else if (nota <= 4.9 && nota >= 2){
+		printf("mi\n");
+	}else if (nota <= 1.9 && nota >= 0.1){
+		printf("ii\n");
+	} else if (nota == 0 ){
+	printf ("sr\n");}
+	else {
+		printf("Nota invalida! Tente novamente");
+	}
+		return 0 ;
+}
