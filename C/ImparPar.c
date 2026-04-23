6ec3f2c89e2924ea036ee61440314419e4d104df
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-23 11:56:16 -0300

    Add files via upload

---
+++ C/ImparPar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main(){
+	
+	te de impar ou par em C
+
+int num9 = 0;
+
+printf("Digite um numero: ");
+scanf("%i", &num9);
+
+if(num9 % 2 == 0){
+ 		printf ("par \\n");
+	} else {
+		printf("impar" );
+	}
+}
+   
