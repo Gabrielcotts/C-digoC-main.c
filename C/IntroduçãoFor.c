2eb7a306cfdb948dd86e210cfe2e4e995eaefb76
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-23 12:06:24 -0300

    Add files via upload

---
+++ C/IntroduçãoFor.c
@@ -0,0 +1,19 @@
+
+
+int main(){
+
+int num[5];
+int i = 0;
+
+	printf("Digite 10 numeros: ");
+	
+	for(i = 0; i <= 10; i++){
+	scanf("%i", &num[i]);	
+	}	
+	printf("Numeros digitados: \n ");
+		for(i = 0; i <= 10; i++){
+	printf ("%i \n", 
+	num[i]);	
+	}	
+	return 0;
+}
