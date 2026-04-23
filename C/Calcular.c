fea627ab5ddf3234ead37dcf808f9a22ba28a2ae
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-23 12:11:29 -0300

    Add files via upload

---
+++ C/Calcular.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main(){
+
+int subtracao(int a, int b){
+	return a - b;
+	}
+	resultado = subtracao(10,3);
+	printf("Resultado da subtracao eh %i", resultado);
+	
+	
+	int multiplicacao(int c, int d){
+		return c * d;
+	}
+	resultado2 = multiplicacao(10,3);
+	printf("\nResultado da multiplicacao eh %i", resultado2);
+
+	
+		int divicao(float e, float f){
+		return e / f;
+	}
+	resultado3 = multiplicacao(10,2);
+	printf("\nResultado da divicao eh %i", resultado3);
+	return 0;
+}
