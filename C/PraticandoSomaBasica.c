14723d7ed83d4a6471c9356fdf11e6f63ce85ac5
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-04-23 11:51:55 -0300

    Add files via upload

---
+++ C/PraticandoSomaBasica.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main(){
+	
+		float num1 = 8;
+	float num2 = 9.2;
+	
+	if( (num1 + num2 ) == 17){
+		printf("Resultado eh 17!");
+	} else {
+		printf("Resultado eh 17,2!");
+	}
+	
