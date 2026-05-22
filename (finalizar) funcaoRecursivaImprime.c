e46de97668e54ac1caf00895d163b376cef2ec44
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-05-21 21:43:22 -0300

    Update (finalizar) funcaoRecursivaImprime.c

--- (finalizar) funcaoRecursivaImprime.c
+++ (finalizar) funcaoRecursivaImprime.c
@@ -1,21 +1,65 @@
 #include <stdio.h>
 
-void imprime(int valor[], int i, int num);
+// Função recursiva
+long fiboRecursivo(long n){
+
+    printf("Entrou na funcao fibo(%ld)\n", n);
+
+  
+    if(n <= 2){
+        printf("Retornando 1 da funcao fibo(%ld)\n", n);
+        return 1;
+    }
+
+    long resultado = fiboRecursivo(n - 1) + fiboRecursivo(n - 2);
+
+    printf("Retornando %ld da funcao fibo(%ld)\n", resultado, n);
+
+    return resultado;
+}
+
+// Função interativa
+long fiboIterativo(long n){
+
+    long anterior = 1;
+    long atual = 1;
+    long proximo;
+    int i;
+
+    if(n <= 2){
+        return 1;
+    }
+
+    for(i = 3; i <= n; i++){
+
+        proximo = anterior + atual;
+
+        anterior = atual;
+
+        atual = proximo;
+    }
+
+    return atual;
+}
 
 int main(){
-	int vet[] = {1,2,3,4,5,6,7,8,9,10};
-	
-	imprime (vet, 0, 9);
-	printf("\n");
-}
 
-void imprime(int valor[], int i, int num){
-	if(i == num){
-		printf("%d, ", valor[i]);
-	
-	} else {
-		imprime(valor, i + 1, num );
-		printf("%d, ", valor[i]);
-	}
-}
+    long numero;
 
+    printf("Digite um numero: ");
+    scanf("%ld", &numero);
+
+    printf("\n=== RECURSIVO ===\n");
+
+    long resultadoRec = fiboRecursivo(numero);
+
+    printf("\nResultado recursivo: %ld\n", resultadoRec);
+
+    printf("\n=== ITERATIVO ===\n");
+
+    long resultadoIt = fiboIterativo(numero);
+
+    printf("Resultado iterativo: %ld\n", resultadoIt);
+
+    return 0;
+}
