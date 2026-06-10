f003fe16b4eb622f13d2fddd475375b3de3e4c5b
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-10 11:58:17 -0300

    Add files via upload

---
+++ C/LogicaC/Formatadores de texto.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("Oi, tudo bem? Tenho 21 anos e programo.\n");
+	
+	printf("Valor inteiro: %d.\n", 10);
+	printf("Valor real: %f.\n", 3.14526);
+	printf("Valor real com apenas duas casas: %.2f.\n", 3.14526);
+	printf("Dado de texto: %c\n", 'a');
+	printf("Dado de texto: %s.\n", "testando");
+	
+	return 0;
+}
