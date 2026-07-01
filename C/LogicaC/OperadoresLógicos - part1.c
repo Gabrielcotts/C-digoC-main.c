0162d3d5ba7dc9b9b12834bfde1aa30627d9a7c3
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-30 21:36:30 -0300

    Add files via upload

---
+++ C/LogicaC/OperadoresLógicos - part1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	
+	int A, B, soma, sub, mult, div;
+	
+	printf("Digite o primeiro valor:\n");
+	scanf("%d", &A);
+	printf("Digite o segundo valor:\n");
+	scanf("%d", &B);
+	
+	soma = A+B;
+	sub = A-B;
+	mult = A*B;
+	div = A/B;
+	
+	printf("Resultados:\n");
+	printf("Soma: %d\n", soma);
+	printf("Subtracao: %d\n", sub);
+	printf("Multiplicacao: %d\n", mult);
+	printf("Divisao: %d\n", div);
+	
+}
